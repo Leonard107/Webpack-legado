93b41e92bd163f9a9eb5446d2e390ddab93e21c4
LEONARD RICHARD <leonardrichard107@gmail.com>
2021-05-19 10:02:34 -0300

    usando o sistema de Módulos do ES2015 sem Babel

--- ex/index.js
+++ ex/index.js
@@ -1,2 +1,9 @@
-const logger = require('./logger');
-logger.info('Usando o padrão CommonJS!')
+//const logger = require('./logger');
+//logger.info('Usando o padrão CommonJS!')
+
+//usando  o Sistema de  Módulos do ES2015 sem Babel
+
+import Pessoa from './pessoa'
+
+const pessoa = new Pessoa('Leonard')
+console.log(pessoa.toString())
